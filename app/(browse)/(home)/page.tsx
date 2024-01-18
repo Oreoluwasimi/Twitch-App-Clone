b0098b5012504c4a9feeb56ba336e7a7a19dd309
Oreoluwa Olajide <oreoluwasimi02@gmail.com>
2024-01-18 13:25:51 +0000

    twitch app clone initial commit

---
+++ app/(browse)/(home)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
+
+export default function Home() {
+  return (
+    
+    // <Button size='lg' variant='destructive'>click me</Button>
+    <div className='flex flex-col gap-y-4'>
+      <h1>Home Page</h1>
+    
+    </div>
+  )
+}
